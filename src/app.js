20dc74c688cfb75386d6af2a07e53bb99e091af8
Jenny Lian <jennylian100@gmail.com>
2015-03-14 11:12:59 -0700

    Jenny Fixed something
    Going forward with firebase

--- src/app.js
+++ src/app.js
@@ -1,10 +1,37 @@
-//require the ui 
 var UI = require('ui');
+var ajax = require('ajax');
 
+// Create a Card with title and subtitle
 var card = new UI.Card({
-  title: 'JL Weather Channel',
-  subtitle: 'Fectching man...'
+  title:'#MFM',
+  subtitle:'Meat Free Monday \^o^/ '
 });
 
-//Display the card now
+// Display the Card
 card.show();
+
+// Construct URL
+var URL = 'https://flickering-fire-8922.firebaseio.com/.json';
+
+// Make the request
+ajax(
+  {
+    url: URL,
+    type: 'json'
+  },
+  function(data) {
+    // Success!
+    console.log("Successfully fetched food data!");
+
+    // Extract data
+    var food = data.food[1].name;
+
+    // Show to user
+    card.subtitle(food);
+    card.body("Jenny likes food");
+  },
+  function(error) {
+    // Failure!
+    console.log('Failed fetching firebase data: ' + error);
+  }
+);
